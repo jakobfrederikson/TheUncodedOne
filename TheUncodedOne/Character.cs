347e7754e931bbb91e3cdc61ce7f7d0e250208af
Jakob <jakobfred1999@gmail.com>
2023-02-28 23:29:08 +1300

    Removed TakeTurn(Action action) due to new IAction

--- TheUncodedOne/Character.cs
+++ TheUncodedOne/Character.cs
@@ -3,20 +3,16 @@
     public abstract class Character
     {
         public abstract string Name { get; }
-        public abstract void TakeTurn(Action action);
     }
 
     public class TrueProgrammer : Character
     {
         public override string Name { get; }
         public TrueProgrammer(string name) => Name = name;
-
-        public override void TakeTurn(Action action) => action.Invoke();
     }
 
     public class Skeleton : Character
     {
         public override string Name => "SKELETON";
-        public override void TakeTurn(Action action) => action.Invoke();
     }
 }
