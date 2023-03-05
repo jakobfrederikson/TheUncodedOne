f453d2714a12a4d40a3115b9b29ae3bf49fe0c74
Jakob <jakobfred1999@gmail.com>
2023-03-05 16:51:32 +1300

    Core Game: The Player Decides completed

--- TheUncodedOne/IPlayer.cs
+++ TheUncodedOne/IPlayer.cs
@@ -13,4 +13,22 @@
             return new AttackAction(character.Attack, game.GetEnemyPartyFor(character).Characters[0]);
         }
     }
+
+    public class HumanPlayer : IPlayer
+    {
+        public IAction ChooseAction(Game game, Character character)
+        {
+            // 1. Character's attack
+            // 2. Do nothing
+            Console.WriteLine($"1 - Standard Attack ({character.Attack.Name})");
+            Console.WriteLine("2 - Do Nothing");
+            int input = Convert.ToInt32(ColouredConsole.Prompt("What do you want to do? "));
+
+            return input switch
+            {
+                1 => new AttackAction(character.Attack, game.GetEnemyPartyFor(character).Characters[0]),
+                2 => new DoNothingAction()
+            };
+        }
+    }
 }
--- TheUncodedOne/Program.cs
+++ TheUncodedOne/Program.cs
@@ -1,24 +1,93 @@
-﻿using TheUncodedOne;
+﻿using System.Xml.Linq;
+using TheUncodedOne;
 
-string name = ColouredConsole.Prompt("What is your name?").ToUpper();
+Console.WriteLine("Select a game mode...");
+Console.WriteLine("1 - Player vs Computer");
+Console.WriteLine("2 - Computer vs Computer");
+Console.WriteLine("3 - Player vs Player");
+int gameChoice = Convert.ToInt32(ColouredConsole.Prompt("What do you want to do? "));
 
-Party heroesParty = new Party(new ComputerPlayer());
-heroesParty.Characters.Add(new TrueProgrammer(name));
+Game game = gameChoice switch
+{
+    1 => PlayerVsComputer(),
+    2 => ComputerVsComputer(),
+    3 => PlayerVsPlayer()
+};
+game.Run();
 
-Party firstBattle = new Party(new ComputerPlayer());
-firstBattle.Characters.Add(new Skeleton());
-Party secondBattle = new Party(new ComputerPlayer());
-secondBattle.Characters.Add(new Skeleton());
-secondBattle.Characters.Add(new Skeleton());
+/* ===============================================================
+                                Methods
+   =============================================================== */
 
-Party thirdBattle = new Party(new ComputerPlayer());
-thirdBattle.Characters.Add(new UncodedOne());
+Game PlayerVsComputer()
+{
+    string name = ColouredConsole.Prompt("What is your name?").ToUpper();
 
-List<Party> allMonsters = new List<Party>() { 
+    Party heroesParty = new Party(new HumanPlayer());
+    heroesParty.Characters.Add(new TrueProgrammer(name));
+
+    Party firstBattle = new Party(new ComputerPlayer());
+    firstBattle.Characters.Add(new Skeleton());
+    Party secondBattle = new Party(new ComputerPlayer());
+    secondBattle.Characters.Add(new Skeleton());
+    secondBattle.Characters.Add(new Skeleton());
+
+    Party thirdBattle = new Party(new ComputerPlayer());
+    thirdBattle.Characters.Add(new UncodedOne());
+
+    List<Party> allMonsters = new List<Party>() {
                                                 firstBattle,
                                                 secondBattle,
                                                 thirdBattle
                                             };
 
-Game game = new Game(heroesParty, allMonsters);
-game.Run();
+    return new Game(heroesParty, allMonsters);
+}
+
+Game ComputerVsComputer()
+{
+    Party heroesParty = new Party(new ComputerPlayer());
+    heroesParty.Characters.Add(new TrueProgrammer("GOOD BOT"));
+
+    Party firstBattle = new Party(new ComputerPlayer());
+    firstBattle.Characters.Add(new Skeleton());
+    Party secondBattle = new Party(new ComputerPlayer());
+    secondBattle.Characters.Add(new Skeleton());
+    secondBattle.Characters.Add(new Skeleton());
+
+    Party thirdBattle = new Party(new ComputerPlayer());
+    thirdBattle.Characters.Add(new UncodedOne());
+
+    List<Party> allMonsters = new List<Party>() {
+                                                firstBattle,
+                                                secondBattle,
+                                                thirdBattle
+                                            };
+
+    return new Game(heroesParty, allMonsters);
+}
+
+Game PlayerVsPlayer()
+{
+    string heroName = ColouredConsole.Prompt("Hero, what is your name?").ToUpper();
+
+    Party heroesParty = new Party(new HumanPlayer());
+    heroesParty.Characters.Add(new TrueProgrammer(heroName));
+
+    Party firstBattle = new Party(new HumanPlayer());
+    firstBattle.Characters.Add(new Skeleton());
+    Party secondBattle = new Party(new HumanPlayer());
+    secondBattle.Characters.Add(new Skeleton());
+    secondBattle.Characters.Add(new Skeleton());
+
+    Party thirdBattle = new Party(new HumanPlayer());
+    thirdBattle.Characters.Add(new UncodedOne());
+
+    List<Party> allMonsters = new List<Party>() {
+                                                firstBattle,
+                                                secondBattle,
+                                                thirdBattle
+                                            };
+
+    return new Game(heroesParty, allMonsters);
+}
